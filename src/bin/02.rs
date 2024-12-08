0661ee932172fb136f97b2d044bde98d83f5a833
Lukas Lehr <lukaslehr@hotmail.com>
2024-12-08 17:51:13 +0100

    fucked up copying the template

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -13,49 +13,11 @@ fn main() -> Result<()> {
 }
 
 fn solve_part1(input: &str) -> Result<i64> {
-    let nums: Vec<Vec<i64>> = input
-        .lines()
-        .filter(|line| !line.is_empty())
-        .map(|line| {
-            line.split_whitespace()
-                .filter_map(|n| n.parse().ok())
-                .collect()
-        })
-        .collect();
-
-        let mut cols: (Vec<i64>, Vec<i64>) = (
-            nums.iter().map(|row: &Vec<i64>| row[0]).collect::<Vec<i64>>(),
-            nums.iter().map(|row: &Vec<i64>| row[1]).collect::<Vec<i64>>()
-        );
-        cols.0.sort();
-        cols.1.sort();
-
-        let total: i64 = cols.0.iter().zip(cols.1.iter()).map(|(a, b)| (a - b).abs()).sum();
-
-    Ok(total)
+    Ok(0)
 }
 
 fn solve_part2(input: &str) -> Result<i64> {
-    let nums: Vec<Vec<i64>> = input
-    .lines()
-    .filter(|line| !line.is_empty())
-    .map(|line| {
-        line.split_whitespace()
-            .filter_map(|n| n.parse().ok())
-            .collect()
-    })
-    .collect();
-
-    let cols: (Vec<i64>, Vec<i64>) = (
-        nums.iter().map(|row: &Vec<i64>| row[0]).collect::<Vec<i64>>(),
-        nums.iter().map(|row: &Vec<i64>| row[1]).collect::<Vec<i64>>()
-    );
-
-    let total: i64 = cols.0.iter().map(|a| {
-        a * (cols.1.iter().filter(|&b| b == a).count() as i64)
-    }).sum();
-
-    Ok(total)
+    Ok(0)
 }
 
 #[cfg(test)]
@@ -63,13 +25,13 @@ mod tests {
     use super::*;
     
     const TEST_INPUT: &str = r#"
-        3   4
-        4   3
-        2   5
-        1   3
-        3   9
-        3   3
-        "#;
+    7 6 4 2 1
+    1 2 7 8 9
+    9 7 6 2 1
+    1 3 2 4 5
+    8 6 4 4 1
+    1 3 6 7 9
+    "#;
     
     #[test]
     fn test_part1() -> Result<()> {
